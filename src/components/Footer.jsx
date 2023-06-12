4e2f14e86867ccf5270752f9b6f4e1fabe8915a7
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2023-06-12 12:18:09 -0600

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,28 @@
 import React from "react";
+import styles from "./Header.module.css";
 import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
 
 const Footer = () => {
   return (
     <footer>
-      <div>
+      <div className={styles.social_container}>
         <h5>Hit us up on our socials!</h5>
-        <div>
-          <a href="https://www.devmountain.com" target="_blank">
+        <div className={styles.social_icons}>
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImFacebook size="1.5em" color="#3f3f3f" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank">
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImInstagram size="1.5em" color="#3f3f3f" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank">
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImTwitter size="1.5em" color="#3f3f3f" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank">
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImGithub size="1.5em" color="#3f3f3f" />
           </a>
         </div>
       </div>
+      {/* <div> */}
       <svg
         width="110"
         height="110"
@@ -37,6 +39,7 @@ const Footer = () => {
           stroke-linejoin="round"
         />
       </svg>
+      {/* </div> */}
     </footer>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BiSearchAlt2 } from "react-icons/bi";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header>
       <h2>Devmountain Eatery</h2>
       <nav>
-          <button>Home</button>
-          <button>Add Recipe</button>
+        <Link to="">
+          <button className={styles.nav_btn}>Home</button>
+        </Link>
+        <Link to="/newRecipe">
+          <button className={styles.nav_btn}>Add Recipe</button>
+        </Link>
       </nav>
     </header>
   );
